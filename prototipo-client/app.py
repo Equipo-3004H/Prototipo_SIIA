a3c92baeacb6976473d0c6816762ee683257e727
Olga Sarahi Rodriguez Valladares <sarahir007@gmail.com>
2026-02-28 14:38:08 -0600

    Grabar audio del navegador con streamlit

--- prototipo-client/app.py
+++ prototipo-client/app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.write("Hello world")
-st.audio_input
+st.header("Monitoreo y detección de lenguaje agresivo", divider=True) 
+
+audio_value = st.audio_input("De click para empezar a grabar.")
+
+if audio_value:
+    st.audio(audio_value)
