c7cc95c5e765aebaf0022c953dfc77b430fd9b62
Olga Sarahi Rodriguez Valladares <sarahir007@gmail.com>
2026-02-28 15:33:53 -0600

    Cambiar componente de audio de streamlit por streamlit WebRTC para streaming en tiempo real

--- prototipo-client/app.py
+++ prototipo-client/app.py
@@ -1,8 +1,26 @@
+import av
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
 
 st.header("Monitoreo y detección de lenguaje agresivo", divider=True) 
 
-audio_value = st.audio_input("De click para empezar a grabar.")
 
-if audio_value:
-    st.audio(audio_value)
+# Control de volumen para el audio
+volume = st.slider("Volumen", 0.0, 2.0, 1.0, 0.1)
+
+def audio_frame_callback(frame: av.AudioFrame) -> av.AudioFrame:
+    # Aplicar el control de volumen a los samples de audio
+    samples = frame.to_ndarray()
+    samples = (volume * samples).astype(samples.dtype)  # type: ignore
+
+    # Crear un nuevo marco de audio con los samples modificados
+    new_frame = av.AudioFrame.from_ndarray(samples, layout=frame.layout.name)
+    new_frame.sample_rate = frame.sample_rate
+    return new_frame
+
+# Iniciar el streamer de audio con la función de callback para procesar los frames de audio
+webrtc_streamer(
+    key="audio",
+    audio_frame_callback=audio_frame_callback,
+    media_stream_constraints={"video": False, "audio": True},
+)
