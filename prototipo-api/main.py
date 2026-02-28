33e8f5821ab3b9e2a495942bffbbc49826c541b8
Olga Sarahi Rodriguez Valladares <sarahir007@gmail.com>
2026-02-28 12:38:48 -0600

    Cambio de version de python, agregar metodo para recibir
    Se cambia la versión de Python para asegurar compatibilidad futura con whisper, se agrego un método que recibe un archivo de audio, esta es la base para recibir el audio del frontend

--- prototipo-api/main.py
+++ prototipo-api/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        }
