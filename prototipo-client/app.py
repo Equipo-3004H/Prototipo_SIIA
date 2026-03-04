88aa395789c99fa04ba0697c5a29a7fc8ca0dc3b
Olga Sarahi Rodriguez Valladares <sarahir007@gmail.com>
2026-03-03 19:57:57 -0600

    quinto ajuste de despliegue

--- prototipo-client/app.py
+++ prototipo-client/app.py
@@ -4,9 +4,27 @@ from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
 st.header("Monitoreo y detección de lenguaje agresivo", divider=True) 
 
-# Configuración de STUN/TURN para WebRTC en producción
+# Configuración de STUN/TURN para WebRTC en producción con múltiples servidores
 rtc_configuration = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun3.l.google.com:19302"]},
+            {"urls": ["stun:stun4.l.google.com:19302"]},
+            {
+                "urls": ["turn:openrelay.metered.ca:80"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
+            {
+                "urls": ["turn:openrelay.metered.ca:443"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
+        ]
+    }
 )
 
 # Control de volumen para el audio
